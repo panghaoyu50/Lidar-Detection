b671a880fec73524ef96ef1b3a07155878ff8b2d
Pang Haoyu <panghaoyu50@gmail.com>
2024-07-26 17:35:52 +1200

    some changes

--- src/lidar_detection/lidar_detection/lidar_data_processor.py
+++ src/lidar_detection/lidar_detection/lidar_data_processor.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs_py.point_cloud2 as pc2
+from std_msgs.msg import String
 import open3d as o3d
 import numpy as np
 
@@ -17,30 +18,33 @@ class PointCloudFilterNode(Node):
             10)
 
         # Publish the filtered PointClound to the /filtered_point_cloud topic
-        self.publisher = self.create_publisher(PointCloud2, '/filtered_point_cloud', 10)
+        # self.publisher = self.create_publisher(PointCloud2, '/filtered_point_cloud', 10)
 
         # Define bounding box with respect to Lidar origin
         # width: 4, length 5, height 2 meters box
-        self.min_bound = np.array([-2, 0, -1])
-        self.max_bound = np.array([2, 5, 1])
+        # self.min_bound = np.array([-2, 0, -1])
+        # self.max_bound = np.array([2, 5, 1])
 
     def point_cloud_callback(self, msg):
     	# Read PointCloud message to [(x,y,z)...] format
-        points = list(pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True))
+        points = pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True)
+        
 
+       	for point in points:
+       		self.get_logger().info(f"{point}")
         # Create a open3d PointCloud and add points to the PointCloud
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(points)
+        # pcd = o3d.geometry.PointCloud()
+        # pcd.points = o3d.utility.Vector3dVector(points)
 
 		# Filter out all the points lie outside of the box
-        bounding_box = o3d.geometry.AxisAlignedBoundingBox(self.min_bound, self.max_bound)
-        cropped_pcd = pcd.crop(bounding_box)
+        # bounding_box = o3d.geometry.AxisAlignedBoundingBox(self.min_bound, self.max_bound)
+        # cropped_pcd = pcd.crop(bounding_box)
 
 		# Create the filtered PointCloud
-        filtered_points = np.asarray(cropped_pcd.points)
-        filtered_msg = pc2.create_cloud_xyz32(msg.header, filtered_points)
+        # filtered_points = np.asarray(cropped_pcd.points)
+        # filtered_msg = pc2.create_cloud_xyz32(msg.header, filtered_points)
 
-        self.publisher.publish(filtered_msg)
+ 	    # self.publisher.publish()
 
 def main(args=None):
     rclpy.init(args=args)
